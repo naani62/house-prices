a8b66613ec21df163454036e8af7c576d14fa5f6
naani62 <139780314+naani62@users.noreply.github.com>
2023-07-19 22:37:32 +0100

    Update housing.py

--- housing.py
+++ housing.py
@@ -89,6 +89,80 @@ def main():
             oceanProximity: Location of the house w.r.t ocean/sea.
             
                 """)
+        if choices=='Predicted House':
+    st.subheader("Predicted House")
+    st.info("expand to see data clearly")
+    if os.path.exists("db.csv"):
+        data = pd.read_csv('db.csv')
+        st.write(data)
+    else:
+        st.error("please try some prediction, then the data will be available here")
+if choices=='About':
+    st.subheader("About Us")
+    info='''
+        A house value is simply more than location and square footage. Like the features that make up a person, an educated party would want to know all aspects that give a house its value.
+
+        We are going to take advantage of all of the feature variables available to use and use it to analyze and predict house prices.
+
+        We are going to break everything into logical steps that allow us to ensure the cleanest, most realistic data for our model to make accurate predictions from.
+
+        - Load Data and Packages
+        - Analyzing the Test Variable (Sale Price)
+        - Multivariable Analysis
+        - Impute Missing Data and Clean Data
+        - Feature Transformation/Engineering
+        - Modeling and Predictions
+    '''
+    st.markdown(info,unsafe_allow_html=True)
+
+if choices=='Visual':
+    st.subheader("Data Visualization")      
+
+    train_data = load_train_data(train_path)
+    test_data = load_test_data(test_path)
+
+
+    if st.checkbox("view dataset colum description"):
+        st.subheader('displaying the column wise stats for the dataset')
+        st.write(train_data.columns)
+        st.write(train_data.describe())
+
+    st.subheader('Correlation b/w dataset columns')
+    corrmatrix = train_data.corr()
+    f,ax = plt.subplots(figsize=(20,9))
+    sns.heatmap(corrmatrix,vmax = .8, annot=True)
+    st.pyplot()
+
+    st.subheader("most correlated features")
+    top_corr = train_data.corr()
+    top_corr_feat = corrmatrix.index[abs(corrmatrix['SalePrice'])>.5]
+    plt.figure(figsize=(10,10))
+    sns.heatmap(train_data[top_corr_feat].corr(), annot=True, cmap="RdYlGn")
+    st.pyplot()
+
+    st.subheader("Comparing Overall Quality vs Sale Price")
+    sns.barplot(train_data.OverallQual, train_data.SalePrice)
+    st.pyplot()
+
+    st.subheader("Pairplot visualization to describe correlation easily")
+    sns.set()
+    cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+    sns.pairplot(train_data[cols], size=2.5)
+    st.pyplot()
+
+    st.subheader("Analyis of Sale Price column in dataset")
+    sns.distplot(train_data['SalePrice'] , fit=norm)# Get the fitted parameters used by the function
+    (mu, sigma) = norm.fit(train_data['SalePrice'])
+    st.write( '\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+
+    plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)], loc='best')
+    plt.ylabel('Frequency')
+    plt.title('SalePrice distribution')
+    st.pyplot()
+
+    fig = plt.figure(figsize=(10,10))
+    res = stats.probplot(train_data['SalePrice'], plot=plt,)
+    st.pyplot()
       
 
               
