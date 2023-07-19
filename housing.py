3ea5bfd5a7b7261ebf603075602655d088ed081b
naani62 <139780314+naani62@users.noreply.github.com>
2023-07-19 22:18:22 +0100

    Update housing.py

--- housing.py
+++ housing.py
@@ -5,11 +5,6 @@ import pandas as pd
 import catboost
 from catboost import CatBoostRegressor
 import streamlit as st 
-from PIL import Image
-
-image = Image.open('sunrise.jpg')
-
-st.image(image, caption='Sunrise by the mountains')
 
 from PIL import Image
 
