794d66f24278f3fc3f6e531e750fd93131bdadb2
naani62 <139780314+naani62@users.noreply.github.com>
2023-07-21 08:21:38 +0100

    Update housing.py

--- housing.py
+++ housing.py
@@ -62,8 +62,6 @@ def main():
         st.text(" The Team")
         st.text("""
           html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit House Prices Prediction ML App </h2>
 
             1. Mohamed Ali Ibrahim, Group Leader who bulding the system
             2. Juwayriya Mohamed Hassan text book with 
@@ -95,7 +93,6 @@ def main():
             oceanProximity: Location of the house w.r.t ocean/sea.
             
                 """)
-           </div>
       
 
               
