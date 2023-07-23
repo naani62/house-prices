414966cbe25eee3f99ce0db5de22042737d90ddf
naani62 <139780314+naani62@users.noreply.github.com>
2023-07-23 21:26:36 +0100

    Update housing.py

--- housing.py
+++ housing.py
@@ -1,108 +1,165 @@
-# Importing necessary libraries
-import numpy as np
-import pickle
+import streamlit as st
+import datetime
 import pandas as pd
-import catboost
-from catboost import CatBoostRegressor
-import streamlit as st 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pickle as pk
+import os
 
-from PIL import Image
+# ml related
+from sklearn import ensemble, tree, linear_model
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.utils import shuffle
+import warnings
+from scipy import stats
+from scipy.stats import norm, skew
 
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
+warnings.filterwarnings('ignore')
 
-def welcome():
-    return "Welcome All"
+train_path = "data/train.csv"
+test_path = "data/test.csv"
+real = "data/RealEstate.csv"
+head = 'images/head.jpg'
 
-def predict_california_house_price(house_age, total_rooms, total_bedrooms, population, households, median_income):
-  
-    """
-    **housingMedianAge:** Median age of a house within a block; a lower number is a newer building
-    **totalRooms:** Total number of rooms within a block
-    **totalBedrooms:** Total number of bedrooms within a block
-    **population:** Total number of people residing within a block
-    **households:** Total number of households, a group of people residing within a home unit, for a block
+@st.cache
+def load_train_data(train_path):
+    return pd.read_csv(train_path)
 
-    """
+@st.cache
+def load_test_data(test_path):
+    return pd.read_csv(test_path)
 
-    prediction = classifier.predict([[house_age, total_rooms, total_bedrooms, population, households, median_income]])
-
-
-    print(prediction)
-    return prediction
+@st.cache
+def load_data(real):
+    return pd.read_csv(real)
 
 
+def save_data(value,res):
+    file = 'db.csv'
+    if not os.path.exists(file):
+        with open(file,'w') as f:
+            f.write("OverallQual,GrLivArea,GarageCars,GarageArea,TotalBsmtSF,fstFlrSF,FullBath,TotRmsAbvGrd,YearBuilt,YearRemodAdd,GarageYrBlt,MasVnrArea,Fireplaces,BsmtFinSF1,Result\n")
+    with open(file,'a') as f:
+        data = f"{OverallQual},{GrLivArea},{GarageCars},{GarageArea},{TotalBsmtSF},{fstFlrSF},{FullBath},{TotRmsAbvGrd},{YearBuilt},{YearRemodAdd},{GarageYrBlt},{MasVnrArea},{Fireplaces},{BsmtFinSF1},{res}\n"        
+        f.write(data)
 
-def main():
-    st.title("California House Price Prediction using machine learning")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit House Prices Prediction ML App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    longitude = st.text_input("longitude","+ -")
-    latitude = st.text_input("latitude","Type Here")
-    house_age = st.text_input("House Age","Type Here")
-    total_rooms = st.text_input("Total Rooms","Type Here")
-    total_bedrooms = st.text_input("Total Bedrooms","Type Here")
-    population = st.text_input("Population","Type Here")
-    households = st.text_input("Households","Type Here")
-    median_income = st.text_input("Median Income","Type Here")
-    median_house_value = st.text_input(" median_house_value","Type Here")
-    ocean_proximity = st.text_input(" ocean_proximity","Type Here")
-   
-    
-    result=""
-    if st.button("Predict"):
-        result = predict_california_house_price(house_age, total_rooms, total_bedrooms, population, households, median_income)
-        formatted_result = "${:.4f}".format(result.item())
-        st.success('The Prediction Price Based on your Requirement is {}'.format(formatted_result))
-    
+st.sidebar.image(head, caption="project on Artificial intelligence",use_column_width=True)
 
-    if st.button("About"):
-        st.text(" House Price Prediction")
-        st.text(" The Team")
-        st.text("""
-            1. Mohamed Ali Ibrahim, Group Leader who bulding the system
-            2. Juwayriya Mohamed Hassan text book with 
-            3. Hassan Abdi Mohamed assisant juwriya
-            4. Bian Abdullahi Said assisanat with mohamed
-         
-        
 
-            Variable descrpition:
 
-            longitude: A measure of how far west a house is; a higher value is farther west.
 
-            latitude: A measure of how far north a house is; a higher value is farther north.
+st.title("House Pricing  prdection using machine learning Analysis")
 
-            housingMedianAge: Median age of a house within a block; a lower number is a newer building.
+menu=["House Prediction","Predicted House","About","Visual"]
+choices=st.sidebar.selectbox("Menu Bar",menu)
 
-            totalRooms: Total number of rooms within a block.
+if choices=='House Prediction':
+    st.subheader("House Prediction")
+    OverallQual=st.selectbox("Select the overall quality(10 being 'Very Excellent' and 1 being 'very poor')",(10,9,8,7,6,5,4,3,2,1))
+    GrLivArea= st.number_input("Enter Ground Floor Living Area (in Sqft)",value=0,min_value=0,format='%d')
+    GarageArea=st.number_input("Enter area of Garage (in Sqft)",value=0.0,format='%f',step=1.0)
+    GarageCars=st.number_input("Number of Cars to be accomodated in garage",min_value=1.0,max_value=10.0,step=1.0,format='%f')
+    TotalBsmtSF=st.number_input("Enter area of Basement (in Sqft)",value=0.0,format='%f',step=1.0)
+    fstFlrSF=st.number_input("Enter area of First Floor (in Sqft)",value=0,format='%d')
+    FullBath=st.number_input("Enter number of Bathrooms",min_value=1,max_value=10,format='%d')
+    TotRmsAbvGrd=st.number_input("Enter number of Rooms",min_value=1,max_value=10,format='%d')
+    years=tuple([i for i in range(1872,2011)])
+    YearBuilt=st.selectbox("Select the overall quality(10 being 'Very Excellent' and 1 being 'very poor')",years)
+    remyears=tuple([i for i in range(1950,2011)])
+    YearRemodAdd=st.selectbox("Select Remodel date (same as construction date if no remodeling or additions)",remyears)
+    garyears=tuple([i for i in range(1872,2011)])
+    garyears=tuple(map(float,garyears))
+    GarageYrBlt=st.selectbox("Select year in which Garage was built)",garyears)
+    MasVnrArea=st.number_input("Masonry veneer area (in Sqft)",value=0.0,format='%f',step=1.0)
+    Fireplaces=st.number_input("Select number of FirePlaces",min_value=1,max_value=10,format='%d')
+    BsmtFinSF1=st.number_input("Enter Basement Finished Area(in Sqft)",value=0,format='%d')
+    submit = st.button('Predict')
+    if submit:
+        st.success("Prediction Done")
+        value=[OverallQual,GrLivArea,GarageCars,GarageArea,TotalBsmtSF,fstFlrSF,FullBath,TotRmsAbvGrd,YearBuilt,YearRemodAdd,GarageYrBlt,MasVnrArea,Fireplaces,BsmtFinSF1]
+        df=pd.DataFrame(value).transpose()
+        # st.dataframe(df)
+        #model=pk.load(open('model & scaler/rfrmodel.pkl','rb'))
+        #scaler=pk.load(open('model & scaler/scale.pkl','rb'))
+        #scaler.transform(df)
+        ans=int(model.predict(df)) * 5
+        st.subheader(f"The price is {ans} (INR) ")
+        save_data(value,ans)
 
-            totalBedrooms: Total number of bedrooms within a block.
+if choices=='Predicted House':
+    st.subheader("Predicted House")
+    st.info("expand to see data clearly")
+    if os.path.exists("db.csv"):
+        data = pd.read_csv('db.csv')
+        st.write(data)
+    else:
+        st.error("please try some prediction, then the data will be available here")
+if choices=='About':
+    st.subheader("About Us")
+    info='''
+        A house value is simply more than location and square footage. Like the features that make up a person, an educated party would want to know all aspects that give a house its value.
 
-            population: Total number of people residing within a block.
+        We are going to take advantage of all of the feature variables available to use and use it to analyze and predict house prices.
 
-            households: Total number of households, a group of people residing within a home unit, for a block.
+        We are going to break everything into logical steps that allow us to ensure the cleanest, most realistic data for our model to make accurate predictions from.
 
-            medianIncome: Median income for households within a block of houses (measured in tens of thousands of US Dollars).
+        - Load Data and Packages
+        - Analyzing the Test Variable (Sale Price)
+        - Multivariable Analysis
+        - Impute Missing Data and Clean Data
+        - Feature Transformation/Engineering
+        - Modeling and Predictions
+    '''
+    st.markdown(info,unsafe_allow_html=True)
 
-            medianHouseValue: Median house value for households within a block (measured in US Dollars).
+if choices=='Visual':
+    st.subheader("Data Visualization")      
 
-            oceanProximity: Location of the house w.r.t ocean/sea.
-            
-                """)
+    train_data = load_train_data(train_path)
+    test_data = load_test_data(test_path)
 
-              
-        st.text("Built with mohamed ali using Streamlit")
-     
-    
-       
 
-if __name__=='__main__':
-    main()
-    
-    
-  
+    if st.checkbox("view dataset colum description"):
+        st.subheader('displaying the column wise stats for the dataset')
+        st.write(train_data.columns)
+        st.write(train_data.describe())
+
+    st.subheader('Correlation b/w dataset columns')
+    corrmatrix = train_data.corr()
+    f,ax = plt.subplots(figsize=(20,9))
+    sns.heatmap(corrmatrix,vmax = .8, annot=True)
+    st.pyplot()
+
+    st.subheader("most correlated features")
+    top_corr = train_data.corr()
+    top_corr_feat = corrmatrix.index[abs(corrmatrix['SalePrice'])>.5]
+    plt.figure(figsize=(10,10))
+    sns.heatmap(train_data[top_corr_feat].corr(), annot=True, cmap="RdYlGn")
+    st.pyplot()
+
+    st.subheader("Comparing Overall Quality vs Sale Price")
+    sns.barplot(train_data.OverallQual, train_data.SalePrice)
+    st.pyplot()
+
+    st.subheader("Pairplot visualization to describe correlation easily")
+    sns.set()
+    cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+    sns.pairplot(train_data[cols], size=2.5)
+    st.pyplot()
+
+    st.subheader("Analyis of Sale Price column in dataset")
+    sns.distplot(train_data['SalePrice'] , fit=norm)# Get the fitted parameters used by the function
+    (mu, sigma) = norm.fit(train_data['SalePrice'])
+    st.write( '\n mu = {:.2f} and sigma = {:.2f}\n'.format(mu, sigma))
+
+    plt.legend(['Normal dist. ($\mu=$ {:.2f} and $\sigma=$ {:.2f} )'.format(mu, sigma)], loc='best')
+    plt.ylabel('Frequency')
+    plt.title('SalePrice distribution')
+    st.pyplot()
+
+    fig = plt.figure(figsize=(10,10))
+    res = stats.probplot(train_data['SalePrice'], plot=plt,)
+    st.pyplot()
