9b2daf124f7648ff5c38054e57e46a312a974440
naani62 <139780314+naani62@users.noreply.github.com>
2023-07-25 19:27:22 +0100

    Update housing.py

--- housing.py
+++ housing.py
@@ -133,11 +133,11 @@ def main():
     st.title("Our Team's Work")
 
     # Load the images
-    image1 = Image.open("WhatsApp Image 2023-07-25 at 6.20.10 PM.jpg")
+    image1 = Image.open("WhatsApp Image 2023-07-25 at 6.20.10 PM.jpeg")
     
-    image2 = Image.open("WhatsApp Image 2023-07-25 at 6.20.10 PM.jpg")
+    image2 = Image.open("WhatsApp Image 2023-07-25 at 6.20.10 PM.jpeg")
    
-    image3 = Image.open("WhatsApp Image 2023-07-25 at 6.20.10 PM.jpg")
+    image3 = Image.open("WhatsApp Image 2023-07-25 at 6.20.10 PM.jpeg")
   
 
     # Display the images with captions
